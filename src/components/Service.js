370158cea5d3bcb88705d5d8306c086e723db91a
sankalp singh <singhsankalp997@gmail.com>
2023-04-14 10:56:14 +0530

    third commit

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,15 +1,33 @@
 import React from 'react'
+import {FaGithub} from "react-icons/fa";
 
 const Service = () => {
   return (
     <div className='service'>
       <div className='container'>
         <div className='service__header'>
+            <div className='common'>
             <h3 className='heading'>SERVICES</h3>
             <h1 className='mainheader'>
                 My Services
             </h1>
-            <p className='mainContent'>Lorem ipsum dolor.Quisque molestie mattis vehicula. Sed fermentum ligula ac finibus mattis. Donec hendrerit dui et arcu facilisis, sit amet efficitur ipsum suscipit. Sed sit amet pulvinar urna. Morbi aliquam, lectus eu venenatis vehicula, orci lorem sollicitudin tellus, in ullamcorper tellus diam at urna</p>
+            <p className='mainContent'>Lorem ipsum dolor.Quisque rerit dui et arcu suscip
+            itus eu venenatis vehillus diam at urna</p>
+            <div className='commonBorder'></div>
+            </div>
+         <div className="row">
+          <div className="col-4">
+            <div className="services__box">
+              <FaGithub className='commonIcons'/>
+              <div className="service__box-header">
+                Web Development
+              </div>
+              <div className="services__box-p">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                 Eveniet reiciendis eiusAutem at architecto dolore. Quo?</div>
+            </div>
+          </div>
+         </div>
         </div>
       </div>
     </div>
