7549b7e4677c207c5018b21d249ad58e86a62a6d
sankalp singh <singhsankalp997@gmail.com>
2023-04-17 09:35:37 +0530

    fifth-commit

--- src/components/About.js
+++ src/components/About.js
@@ -6,12 +6,38 @@ const About = () => {
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum standard dummy.'})
   return (
     <div className='about'>
-        <div className='container'></div>
+        <div className='container'>
          <div className='common'>
             <h1 className='mainheader'>{header.subHeader}</h1>
             <p className='mainContent'>{header.text}</p>
-            <div className='commonBorder'>
+            <div className='commonBorder'></div>
+      </div>
+      <div className="row h-650 alignCentre">
+        <div className="col-6 ">
+            <div className="about__image">
+                <img src="/images/sankalp2.png" alt="man" />
             </div>
+        </div>
+        <div className="col-6">
+            <div className="about__info">
+                <h1>Hi There</h1>
+                <div className="about__info-p1">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit
+                    . Perferendis iusto libero incidunt nam, optio
+                     in tenetur odit? Quae sed nesciunt possimtur officia.
+                </div>
+                <div className="about__info-p2">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing
+                     elit. Nobis, nesciunt, maiorec.
+                </div>
+                <div className="info__contacts">
+                    <div className="row">
+                        <div className="col-6"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
       </div>
     </div>
   )
