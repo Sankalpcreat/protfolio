99887ad549b76d604fa5cf358d2cdba15291921d
sankalp singh <singhsankalp997@gmail.com>
2023-04-21 17:47:02 +0530

    sixth-commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,23 +10,29 @@ const Contact = () => {
   return (
    <div className="contact">
     <div className="container">
-    <div className="aboutSection">
-        <div className="aboutSection-logo">
-            <img src="/images/logo.png" alt="logo" />
+    <div className="contactSection">
             <div className="row justifyCenter">
                 <div className="col-6">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, enim quaerat dicta modi corrupti obcaecati ullam odio optio iure vitae nesciunt dolorum officia quas a saepe sapiente, iusto possimus natus!
-                    <div className="contactcircle">
-                        <ul>
+                <div className="contactSection-logo">
+            <img src="/images/logo.png" alt="logo" />
+            </div>
+                   <p>
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Doloremque, enim quaerat dicta modi corrupti
+                      obcaecati ullam odio optio iure vitae nesciunt
+                       dolorum officia quas a saepe sapiente, iusto possimus natus!
+                   </p>
+                    <ul className="contactcircle">
+                       
                             <li><FaFacebookF/></li>
                             <li><FaLinkedinIn/></li>
                             <li><FaGithub/></li>
                             <li><FaApple/></li>
-                        </ul>
-                    </div>
+                        
+                    </ul>
                 </div>
             </div>
-        </div>
+        
     </div>
       </div>
     </div>
